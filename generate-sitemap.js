9497d1efb4b7850b71d3b94c43d23db8c0eec77a
MorusPatre <huzzlepanda@gmail.com>
2025-07-04 17:55:54 +0200

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,34 +1,65 @@
 const fs = require('fs');
 const path = require('path');
 
-const galleryDataPath = path.join(__dirname, 'the-witcher', 'gallery-data.json');
-const sitemapPath = path.join(__dirname, 'the-witcher', 'sitemap.xml');
-const baseUrl = 'https://mouseia.com/the-witcher/';
+// Define paths
+const witcherDataPath = path.join(__dirname, 'the-witcher', 'gallery-data.json');
+const sitemapPath = path.join(__dirname, 'sitemap.xml'); // Changed to root directory
+const baseUrl = 'https://mouseia.com';
 
-console.log('Reading gallery data...');
-const galleryData = JSON.parse(fs.readFileSync(galleryDataPath, 'utf8'));
-console.log(`Found ${galleryData.length} items in the gallery.`);
+// --- Functions to build URL entries ---
 
-const sitemapEntries = galleryData.map(item => {
-  // Create a URL-friendly name from the filename for the hash
-  const urlHash = encodeURIComponent(item.filename);
-  return `
+function buildStaticUrl(urlPath, changefreq, priority) {
+    return `
   <url>
-    <loc>${baseUrl}#${urlHash}</loc>
+    <loc>${baseUrl}${urlPath}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+  </url>`;
+}
+
+function buildGalleryUrls(galleryData, galleryPath) {
+    return galleryData.map(item => {
+        const urlHash = encodeURIComponent(item.filename);
+        return `
+  <url>
+    <loc>${baseUrl}${galleryPath}#${urlHash}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>`;
-});
+    }).join('');
+}
+
+
+// --- Main sitemap generation ---
+
+console.log('Starting sitemap generation...');
+
+let sitemapEntries = [];
+
+// Add the main Witcher gallery page
+sitemapEntries.push(buildStaticUrl('/the-witcher/', 'weekly', '0.9'));
+
+// Add all individual Witcher images
+try {
+    const witcherData = JSON.parse(fs.readFileSync(witcherDataPath, 'utf8'));
+    sitemapEntries.push(buildGalleryUrls(witcherData, '/the-witcher/'));
+    console.log(`Added ${witcherData.length + 1} URLs for The Witcher gallery.`);
+} catch (error) {
+    console.error('Could not read or parse The Witcher gallery data.', error);
+}
+
+// Future-proofing: Add another gallery here if you create one
+// For example:
+// sitemapEntries.push(buildStaticUrl('/house-of-the-dragon/', 'weekly', '0.9'));
+// const hotdData = JSON.parse(fs.readFileSync(path.join(__dirname, 'house-of-the-dragon', 'gallery-data.json'), 'utf8'));
+// sitemapEntries.push(buildGalleryUrls(hotdData, '/house-of-the-dragon/'));
+
+// --- Assemble and write the final sitemap ---
 
 const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
   ${sitemapEntries.join('')}
 </urlset>`;
 
