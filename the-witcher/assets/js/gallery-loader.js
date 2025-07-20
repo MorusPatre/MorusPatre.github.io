f3d8a42ecf00ea7294f97b15e63b20d4a35ab610
MorusPatre <huzzlepanda@gmail.com>
2025-07-20 02:45:02 +0200

    Update gallery-loader.js

--- the-witcher/assets/js/gallery-loader.js
+++ the-witcher/assets/js/gallery-loader.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
             galleryContainer.innerHTML = 'Error loading gallery. Please check the console.';
         });
 
-    function truncateFilename(filename, maxLength = 60) {
+    function truncateFilename(filename, maxLength = 50) {
         if (filename.length <= maxLength) {
             return filename;
         }
